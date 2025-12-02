2eab2b6349af1777c650d682302e793d34cef6b6
Sebastian <sebastian.juelmilo@outlook.dk>
2025-12-02 08:21:04 +0100

    testing

--- test/exercise5/NewTest.java
+++ test/exercise5/NewTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 
 public class NewTest {
     @Test
-    public void testObserver() {
+    public void testObserverAddBook() {
         BookLocation locA1 = new BookLocation("A", 1);
 
         Book b1 = new Book("111", "Clean Code", "Robert Martin", locA1);
@@ -22,6 +22,19 @@ public class NewTest {
         lib.addBook(b2);
 
         assertEquals(2, locA1.getBooks().size());
+    }
+
+    @Test
+    public void testObserverRemoveBook() {
+        BookLocation locA1 = new BookLocation("A", 1);
+
+        Book b1 = new Book("111", "Clean Code", "Robert Martin", locA1);
+        Book b2 = new Book("222", "Effective Java", "Joshua Bloch", locA1);
+
+        Library lib = new Library(new SearchByTitle());
+
+        lib.addBook(b1);
+        lib.addBook(b2);
 
         lib.removeBook(b1);
 
